92825c74b3ae9b380e508b2a69798fb3d2844b37
xuxiaocheng <2762267080@qq.com>
2024-01-22 00:42:08 +0800

    feat(lib): 重构异步的bools读写

--- src/asynchronous/reader.rs
+++ src/asynchronous/reader.rs
@@ -79,7 +79,7 @@ macro_rules! read_raw_future {
                 buf: $buf,
             }
         }
-        impl<'a, R: $crate::asynchronous::AsyncVariableReadable + Unpin + ?Sized> std::future::Future for $future<'a, R> {
+        impl<'a, R: $crate::asynchronous::AsyncVariableReadable + Unpin> std::future::Future for $future<'a, R> {
             type Output = std::io::Result<$primitive>;
 
             fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
@@ -112,7 +112,7 @@ macro_rules! read_raw_size_future {
                 buf: OwnedReadBuf128,
             }
         }
-        impl<'a, R: $crate::asynchronous::AsyncVariableReadable + Unpin + ?Sized> std::future::Future for $future<'a, R> {
+        impl<'a, R: $crate::asynchronous::AsyncVariableReadable + Unpin> std::future::Future for $future<'a, R> {
             type Output = std::io::Result<$primitive>;
 
             fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
@@ -207,6 +207,74 @@ macro_rules! define_read_raw_func {
 #[cfg(feature = "async_raw")]
 define_read_raw_futures!();
 
+#[cfg(feature = "async_bools")]
+macro_rules! read_bools_future {
+    ($future: ident, $n: literal) => {
+        $crate::pin_project_lite::pin_project! {
+            #[derive(Debug)]
+            #[project(!Unpin)]
+            #[must_use = "futures do nothing unless you `.await` or poll them"]
+            pub struct $future<'a, R: ?Sized> {
+                #[pin]
+                reader: &'a mut R,
+            }
+        }
+        impl<'a, R: $crate::asynchronous::AsyncVariableReadable + Unpin> std::future::Future for $future<'a, R> {
+            type Output = std::io::Result<[bool; $n]>;
+
+            fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+                const MAX: u8 = ((1 << ($n - 1)) - 1 << 1) + 1; // (1 << $n) - 1
+                let mut me = self.project();
+                let b = ready!(R::poll_read_single(Pin::new(&mut *me.reader), cx))?;
+                if b > MAX {
+                    return  Poll::Ready(Err(Error::new(ErrorKind::InvalidData, format!("Invalid bools at {}.", stringify!($func)))));
+                }
+                let mut bools = [false; $n];
+                for i in 0..$n {
+                    bools[i] = b & (1 << i) != 0;
+                }
+                Poll::Ready(Ok(bools))
+            }
+        }
+
+    };
+}
+#[cfg(feature = "async_bools")]
+macro_rules! read_bools_func {
+    ($func: ident, $future: ident) => {
+        #[inline]
+        fn $func(&mut self) -> $future<Self> where Self: Unpin {
+            $future { reader: self }
+        }
+    };
+}
+#[cfg(feature = "async_bools")]
+macro_rules! define_read_bools_futures {
+    () => {
+        read_bools_future!(ReadBools2, 2);
+        read_bools_future!(ReadBools3, 3);
+        read_bools_future!(ReadBools4, 4);
+        read_bools_future!(ReadBools5, 5);
+        read_bools_future!(ReadBools6, 6);
+        read_bools_future!(ReadBools7, 7);
+        read_bools_future!(ReadBools8, 8);
+    };
+}
+#[cfg(feature = "async_bools")]
+macro_rules! define_read_bools_func {
+    () => {
+        read_bools_func!(read_bools_2, ReadBools2);
+        read_bools_func!(read_bools_3, ReadBools3);
+        read_bools_func!(read_bools_4, ReadBools4);
+        read_bools_func!(read_bools_5, ReadBools5);
+        read_bools_func!(read_bools_6, ReadBools6);
+        read_bools_func!(read_bools_7, ReadBools7);
+        read_bools_func!(read_bools_8, ReadBools8);
+    };
+}
+#[cfg(feature = "async_bools")]
+define_read_bools_futures!();
+
 pub trait AsyncVariableReader: AsyncVariableReadable {
     #[inline]
     fn read_single(&mut self) -> ReadSingle<Self> where Self: Unpin {
@@ -226,8 +294,8 @@ pub trait AsyncVariableReader: AsyncVariableReadable {
     #[cfg(feature = "async_raw")]
     define_read_raw_func!();
 
-    // #[cfg(feature = "async_bools")]
-    // bools::define_bools_read!();
+    #[cfg(feature = "async_bools")]
+    define_read_bools_func!();
 
     // #[cfg(feature = "async_varint")]
     // varint::define_varint_read!();
--- src/asynchronous/writer.rs
+++ src/asynchronous/writer.rs
@@ -71,13 +71,13 @@ macro_rules! write_raw_future {
             #[derive(Debug)]
             #[project(!Unpin)]
             #[must_use = "futures do nothing unless you `.await` or poll them"]
-            pub struct $future<'a, W: Unpin> where W: ?Sized {
+            pub struct $future<'a, W: ?Sized> {
                 #[pin]
                 writer: &'a mut W,
                 buf: $buf,
             }
         }
-        impl<'a, W: $crate::asynchronous::AsyncVariableWritable + Unpin + ?Sized> std::future::Future for $future<'a, W> {
+        impl<'a, W: $crate::asynchronous::AsyncVariableWritable + Unpin> std::future::Future for $future<'a, W> {
             type Output = std::io::Result<usize>;
 
             fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
@@ -104,13 +104,13 @@ macro_rules! write_raw_size_future {
             #[derive(Debug)]
             #[project(!Unpin)]
             #[must_use = "futures do nothing unless you `.await` or poll them"]
-            pub struct $future<'a, W: Unpin> where W: ?Sized {
+            pub struct $future<'a, W: ?Sized> {
                 #[pin]
                 writer: &'a mut W,
                 buf: OwnedWriteBuf128,
             }
         }
-        impl<'a, W: $crate::asynchronous::AsyncVariableWritable + Unpin + ?Sized> std::future::Future for $future<'a, W> {
+        impl<'a, W: $crate::asynchronous::AsyncVariableWritable + Unpin> std::future::Future for $future<'a, W> {
             type Output = std::io::Result<usize>;
 
             fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
@@ -167,7 +167,7 @@ macro_rules! define_write_raw_futures {
     };
 }
 #[cfg(feature = "async_raw")]
-macro_rules! define_read_raw_func {
+macro_rules! define_write_raw_func {
     () => {
         write_raw_func!(u8, write_u8_raw, WriteU8RawNe, to_ne_bytes, OwnedWriteBuf8);
         write_raw_func!(i8, write_i8_raw, WriteI8RawNe, to_ne_bytes, OwnedWriteBuf8);
@@ -205,7 +205,70 @@ macro_rules! define_read_raw_func {
 #[cfg(feature = "async_raw")]
 define_write_raw_futures!();
 
+#[cfg(feature = "async_bools")]
+macro_rules! write_bools_future {
+    ($future: ident) => {
+        $crate::pin_project_lite::pin_project! {
+            #[derive(Debug)]
+            #[project(!Unpin)]
+            #[must_use = "futures do nothing unless you `.await` or poll them"]
+            pub struct $future<'a, W: ?Sized> {
+                #[pin]
+                writer: &'a mut W,
+                b: u8,
+            }
+        }
+        impl<'a, W: $crate::asynchronous::AsyncVariableWritable + Unpin> std::future::Future for $future<'a, W> {
+            type Output = std::io::Result<usize>;
 
+            fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+                let mut me = self.project();
+                W::poll_write_single(Pin::new(&mut *me.writer), cx, *me.b)
+            }
+        }
+    };
+}
+#[cfg(feature = "async_bools")]
+macro_rules! write_bools_func {
+    ($func: ident, $future: ident, $n: literal) => {
+        #[inline]
+        fn $func(&mut self, bools: [bool; $n]) -> $future<Self> where Self: Unpin {
+            let mut b = 0;
+            for i in 0..$n {
+                if bools[i] {
+                    b |= 1 << i;
+                }
+            }
+            $future { writer: self, b }
+        }
+    };
+}
+#[cfg(feature = "async_bools")]
+macro_rules! define_write_bools_futures {
+    () => {
+        write_bools_future!(WriteBools2);
+        write_bools_future!(WriteBools3);
+        write_bools_future!(WriteBools4);
+        write_bools_future!(WriteBools5);
+        write_bools_future!(WriteBools6);
+        write_bools_future!(WriteBools7);
+        write_bools_future!(WriteBools8);
+    };
+}
+#[cfg(feature = "async_bools")]
+macro_rules! define_write_bools_func {
+    () => {
+        write_bools_func!(write_bools_2, WriteBools2, 2);
+        write_bools_func!(write_bools_3, WriteBools3, 3);
+        write_bools_func!(write_bools_4, WriteBools4, 4);
+        write_bools_func!(write_bools_5, WriteBools5, 5);
+        write_bools_func!(write_bools_6, WriteBools6, 6);
+        write_bools_func!(write_bools_7, WriteBools7, 7);
+        write_bools_func!(write_bools_8, WriteBools8, 8);
+    };
+}
+#[cfg(feature = "async_bools")]
+define_write_bools_futures!();
 
 pub trait AsyncVariableWriter: AsyncVariableWritable {
     #[inline]
@@ -224,10 +287,10 @@ pub trait AsyncVariableWriter: AsyncVariableWritable {
     }
 
     #[cfg(feature = "async_raw")]
-    define_read_raw_func!();
+    define_write_raw_func!();
 
-//     #[cfg(feature = "async_bools")]
-//     bools::define_bools_write!();
+    #[cfg(feature = "async_bools")]
+    define_write_bools_func!();
 
 //     #[cfg(feature = "async_varint")]
 //     varint::define_varint_write!();
