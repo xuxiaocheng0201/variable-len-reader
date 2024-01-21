18c2e83fe9c2c771dd92633cd2973ecbe2755f6c
xuxiaocheng <2762267080@qq.com>
2024-01-21 16:00:58 +0800

    feat(lib): 添加自己的ReadBuf/WriteBuf

---
+++ src/bufs.rs
@@ -0,0 +1,143 @@
+use bytes::buf::UninitSlice;
+
+pub struct ReadBuf<'a> {
+    buf: &'a mut [u8],
+    filled: usize,
+}
+
+impl<'a> ReadBuf<'a> {
+    pub fn new(buf: &'a mut [u8]) -> ReadBuf<'a> {
+        ReadBuf {
+            buf,
+            filled: 0,
+        }
+    }
+
+    pub fn advance(&mut self, cnt: usize) {
+        let new = self.filled.checked_add(cnt).expect("filled overflow");
+        assert!(
+            new <= self.buf.len(),
+            "filled must not become larger than buf.len()"
+        );
+        self.filled = new;
+    }
+
+    pub fn buf(&self) -> &[u8] {
+        self.buf
+    }
+
+    pub fn buf_mut(&mut self) -> &mut [u8] {
+        self.buf
+    }
+
+    pub fn filled(&self) -> usize {
+        self.filled
+    }
+
+    pub fn remaining(&self) -> usize {
+        self.buf.len() - self.filled
+    }
+
+    pub fn put(&mut self, val: u8) {
+        assert!(
+            self.remaining() >= 1,
+            "remaining() must large than 1"
+        );
+        self.buf[self.filled] = val;
+        self.filled += 1;
+    }
+
+    pub fn put_slice(&mut self, slice: &[u8]) {
+        assert!(
+            self.remaining() >= slice.len(),
+            "buf.len() must fit in remaining()"
+        );
+        self.buf[self.filled..self.filled + slice.len()].copy_from_slice(slice);
+        self.filled += slice.len();
+    }
+}
+
+#[cfg(feature = "bytes")]
+unsafe impl<'a> bytes::BufMut for ReadBuf<'a> {
+    fn remaining_mut(&self) -> usize {
+        self.remaining()
+    }
+
+    unsafe fn advance_mut(&mut self, cnt: usize) {
+        self.advance(cnt)
+    }
+
+    fn chunk_mut(&mut self) -> &mut UninitSlice {
+        UninitSlice::new(self.buf_mut())
+    }
+}
+
+pub struct WriteBuf<'a> {
+    buf: &'a [u8],
+    read: usize,
+}
+
+impl<'a> WriteBuf<'a> {
+    pub fn new(buf: &'a [u8]) -> WriteBuf<'a> {
+        WriteBuf {
+            buf,
+            read: 0,
+        }
+    }
+
+    pub fn skip(&mut self, cnt: usize) {
+        let new = self.read.checked_add(cnt).expect("read overflow");
+        assert!(
+            new <= self.buf.len(),
+            "read must not become larger than buf.len()"
+        );
+        self.read = new;
+    }
+
+    pub fn buf(&self) -> &[u8] {
+        self.buf
+    }
+
+    pub fn read(&self) -> usize {
+        self.read
+    }
+
+    pub fn remaining(&self) -> usize {
+        self.buf.len() - self.read
+    }
+
+    pub fn get(&mut self) -> u8 {
+        assert!(
+            self.remaining() >= 1,
+            "remaining() must large than 1"
+        );
+        let val = self.buf[self.read];
+        self.read += 1;
+        val
+    }
+
+    pub fn get_slice(&mut self, len: usize) -> &[u8] {
+        assert!(
+            self.remaining() >= len,
+            "remaining() must large than len"
+        );
+        let slice = &self.buf[self.read..self.read + len];
+        self.read += len;
+        slice
+    }
+}
+
+#[cfg(feature = "bytes")]
+impl<'a> bytes::Buf for WriteBuf<'a> {
+    fn remaining(&self) -> usize {
+        self.remaining()
+    }
+
+    fn chunk(&self) -> &[u8] {
+        self.buf
+    }
+
+    fn advance(&mut self, cnt: usize) {
+        self.skip(cnt)
+    }
+}
