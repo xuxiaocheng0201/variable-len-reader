67d51b34b288828ea5b6fefb7a5d52118a52e5e1
xuxiaocheng <2762267080@qq.com>
2024-03-18 22:14:54 +0800

    feat(lib): 添加bytes sync支持

--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -1,5 +1,8 @@
 #[cfg(all(feature = "sync", feature = "std"))]
-mod std;
+pub mod std;
 
-#[cfg(all(feature = "async", feature = "tokio"))]
-mod tokio;
+#[cfg(all(feature = "sync", feature = "bytes-comp"))]
+pub mod bytes;
+
+#[cfg(all(feature = "async", feature = "tokio-comp"))]
+pub mod tokio;
