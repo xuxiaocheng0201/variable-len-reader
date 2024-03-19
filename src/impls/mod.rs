7de919cfa93a936eb1c63a0618f7cf668c8b3bfe
xuxiaocheng <2762267080@qq.com>
2024-03-19 14:09:19 +0800

    fix(Cargo): 修正features

--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -1,5 +1,5 @@
-#[cfg(all(feature = "sync", feature = "std"))]
+#[cfg(all(feature = "sync", feature = "std-comp"))]
 mod std;
 
-#[cfg(all(feature = "async", feature = "tokio"))]
+#[cfg(all(feature = "async", feature = "tokio-comp"))]
 mod tokio;
